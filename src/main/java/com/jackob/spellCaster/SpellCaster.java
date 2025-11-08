8fc5eacc3de6f98e37179eacffec9638d77bfdc0
Jackob2004 <zjakub2004@gmail.com>
2025-11-08 16:21:14 +0100

    Add cast listener
    Integrate listener with cast manager.

--- src/main/java/com/jackob/spellCaster/SpellCaster.java
+++ src/main/java/com/jackob/spellCaster/SpellCaster.java
@@ -1,13 +1,16 @@
 package com.jackob.spellCaster;
 
+import com.jackob.spellCaster.listener.CastListener;
+import com.jackob.spellCaster.manager.CastManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SpellCaster extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        CastManager castManager = new CastManager();
 
+        getServer().getPluginManager().registerEvents(new CastListener(castManager), this);
     }
 
     @Override
---
+++ src/main/java/com/jackob/spellCaster/listener/CastListener.java
@@ -0,0 +1,33 @@
+package com.jackob.spellCaster.listener;
+
+import com.jackob.spellCaster.MouseClick;
+import com.jackob.spellCaster.manager.CastManager;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class CastListener implements Listener {
+
+    private final CastManager castManager;
+
+    public CastListener(CastManager castManager) {
+        this.castManager = castManager;
+    }
+
+    @EventHandler
+    public void onWandClick(PlayerInteractEvent e) {
+        final Player player = e.getPlayer();
+
+        if (player.getInventory().getItemInMainHand().getType() != Material.STICK) return;
+
+        if (e.getAction() == Action.RIGHT_CLICK_BLOCK || e.getAction() == Action.RIGHT_CLICK_AIR) {
+            castManager.updateCombination(player, MouseClick.RIGHT);
+        } else {
+            castManager.updateCombination(player, MouseClick.LEFT);
+        }
+    }
+
+}
